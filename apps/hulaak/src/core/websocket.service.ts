c7d4d08ab788a2da211c0760e0397407eb75aa0b
Santosh Shrestha <santosh@rumsan.com>
2024-09-10 12:16:48 +0545

    ws lifecycle events

--- apps/hulaak/src/core/websocket.service.ts
+++ apps/hulaak/src/core/websocket.service.ts
@@ -1,19 +1,45 @@
+import { Injectable, Logger } from '@nestjs/common';
 import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
   WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import { on } from 'events';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
+  onConnection: (socket) => {
+    console.log('Client connected:', socket.id);
+  },
 }) // This will use the same port as the HTTP server
-export class WebsocketService {
+export class WebsocketService
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = new Logger(WebsocketService.name);
   @WebSocketServer()
   server: Server;
+  private clients: Set<Socket> = new Set();
+
+  afterInit(server: Server) {
+    this.logger.log('WebSocket Gateway initialized');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+    this.clients.add(client);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+    this.clients.delete(client);
+  }
 
   broadcast(ev: string, payload: any): void {
     this.server.emit(ev, payload); // Emit to all connected clients
@@ -21,13 +47,12 @@ export class WebsocketService {
 
   @SubscribeMessage('message')
   handleMessage(client: Socket, payload: any): string {
-    console.log('Message received:', payload);
     return 'Message received: ' + payload;
   }
 
   @SubscribeMessage('broadcast')
   handleBroadcastMessage(client: Socket, payload: any): void {
-    console.log('Broadcasting message:', payload);
     this.server.emit('broadcast', payload); // Emit to all connected clients
+    console.log(client.id);
   }
 }
